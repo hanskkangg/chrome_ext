baa28fcdca0165e5cf33f1f2616fda47a6bc349c
hans kang <hans.kkang@gmail.com>
2025-02-01 15:25:03 -0500

    added random images

---
+++ content.js
@@ -0,0 +1,10 @@
+let willSmithImages = [
+    "https://en.wikipedia.org/wiki/File:%E3%83%AF%E3%82%AD%E3%83%B320120701.JPG",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucepets.com%2F12-types-of-goldfish-5525618&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABAI",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Fanimal%2Fgoldfish&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABAR",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cnn.com%2F2021%2F07%2F14%2Fus%2Ffootball-sized-goldfish-minnesota-lake-scn-trnd%2Findex.html&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABAa",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D%2522gold%2Bfish%2522&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABAj",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fscience4fun.info%2Fgoldfish%2F&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABA1",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucepets.com%2F12-types-of-goldfish-5525618&psig=AOvVaw0NG3Gb1AL7H1PPrfZUoU5o&ust=1738527783663000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMiN4-mmo4sDFQAAAAAdAAAAABA-",
+];
+
